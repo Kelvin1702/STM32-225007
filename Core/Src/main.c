39ef04a80f7fe0e03a8af5f301d3e7ed026673a5
Kelvin1702 <anhkhoangonhat2003@gmail.com>
2025-07-23 23:22:39 +0700

    jshkjhjksh

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -6,6 +6,7 @@
  */
 #include "main.h"
 #define GPIOD_BASE_ADDR  0x40020C00
+#define EXTI_BASE_ADDR  0x40013C00
 void LedInit()
 {
 	__HAL_RCC_GPIOD_CLK_ENABLE();
@@ -22,29 +23,53 @@ void ButtonInit()
 	*GPIOA_MODER &= ~(0b11 << 0);
 }
 
-void LedCtrl(int on_off)
+typedef enum (
+		LED_GREEN = 12;
+        LED_ORANGE ;
+        LED_RED ;
+        LED_BLUE;
+
+) LED_t;
+
+void LedCtrl(LED_t led, int on_off)
 {
 #if 0
 	uint32_t* GPIOD_MODER = (uint32_t*)(GPIOD_BASE_ADDR + 0x14);
 	if(on_off == 1)
 	{
-		*GPIOD_MODER |= (1 << 12);
+		*GPIOD_MODER |= (1 << led);
 	}
 	else
 	{
-		*GPIOD_MODER &= ~(1<<12);
+		*GPIOD_MODER &= ~(1<<led);
 	}
 #else
 	uint32_t* GPIOD_BSRR = (uint32_t*)(GPIOD_BASE_ADDR + 0x18);
 	if(on_off == 1)
 	{
-		*GPIOD_BSRR |= (1<<12);
+		*GPIOD_BSRR |= (1<<led);
 	}
 	else
 	{
-		*GPIOD_BSRR |= (1<<28);
+		*GPIOD_BSRR |= (1<<led+16);
 	}
 #endif
+}
+
+char ButtonState()
+{
+  uint32_t* GPIOA_IDR = (uint32_t*)(GPIOA_BASE_ADDR + 0x10);
+  return (*GPIOA_IDR >> 0) & 1;
+}
+
+
+void EXTI0Init()
+{
+ uint32_t* EXTI_RSTR = (uint32_t*)(EXTI_BASE_ADDR + 0x08);
+ *EXTI_RSTR |= (1 <<0);
+ uint32_t* EXTI_FSTR = (uint32_t*)(EXTI_BASE_ADDR + 0x0C);
+ uint32_t* EXTI_IMR = (uint32_t*)(EXTI_BASE_ADDR + 0x00);
+ *EXTI_IMR |=
 }
 
 int main()
